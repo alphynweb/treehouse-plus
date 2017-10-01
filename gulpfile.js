a7bb9070e5ac2c020a0780dba6a2bcecec67b718
alphynweb <tom@alphynweb.co.uk>
2017-10-02 00:49:40 +0100

    Set gulp babel up for promises

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,13 @@
 var gulp = require('gulp');
 var babel = require('gulp-babel');
 
-gulp.task('build', function() {
+gulp.task('babel', function() {
+   gulp.src('scripts/thp_fields.js')
+           .pipe(babel())
+           .pipe(gulp.dest('test'));
+});
+
+gulp.task('default', function() {
     
 });
 
